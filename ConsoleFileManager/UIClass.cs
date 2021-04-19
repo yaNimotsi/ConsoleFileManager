b4c3e1444ff8c3e68329345d3e1b32691aa5c634
Istomin.I.A <Istomin.I.A@Z4-ISTOMINIA>
2021-04-19 11:52:35 +0300

    SectionPage print

--- ConsoleFileManager/UIClass.cs
+++ ConsoleFileManager/UIClass.cs
@@ -16,77 +16,78 @@ namespace ConsoleFileManager
         private int _countRowToView;
         private string _journalPath;
 
+        private List<string> buffer;
+        private int numPage = 0;
+
         public UIClass()
         {
             Console.CursorVisible = false;
-            Console.SetWindowSize(120, 41);
-            Console.SetBufferSize(120, 41);
+            
+            Console.SetWindowSize(120, 60);
+            Console.SetBufferSize(120, 60);
+            
             Console.ForegroundColor = ConsoleColor.Gray;
             Console.BackgroundColor = ConsoleColor.Black;
 
+            Console.Title = "ConsoleFileManager";
 
+            ReadAppSettings();
 
-            var appSettings = ConfigurationManager.AppSettings;
+            if (_userLastPath.Length > 0)
+                PrintFolderContent(_userLastPath);
+            else
+                PrintFolderContent(_firstPath);
+        }
 
-            if (appSettings.Count == 0)
+        /// <summary>
+        /// Чтение и назначение полям класса первичных настроек приложения
+        /// </summary>
+        private void ReadAppSettings()
+        {
+            var appSettings = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            var settings = appSettings.AppSettings.Settings;
+            if (settings.Count == 0)
             {
                 Console.WriteLine("AppSetting is empty");
             }
             else
             {
-                _firstPath = appSettings["FirstPath"];
-                _userLastPath = appSettings["LastUserPath"];
-                _countRowToView = Convert.ToInt32(appSettings["CountRowToView"]);
-                _journalPath = appSettings["JournalPath"];
-                var asd = appSettings["NewSetting"];
+                foreach (KeyValueConfigurationElement key in settings)
+                {
+                    switch (key.Key)
+                    {
+                        case "FirstPath":
+                            _firstPath = key.Value;
+                            break;
+                        case "LastUserPath":
+                            _userLastPath = key.Value;
+                            break;
+                        case "CountRowToView":
+                            _countRowToView = Convert.ToInt32(key.Value);
+                            break;
+                        case "JournalPath":
+                            _journalPath = key.Value;
+                            break;
+                    };
+
+                }
 
             }
-
-
-            UpdateAppConfigFile("LastUserPath", @"C\");
-            UpdateAppConfigFile("NewSetting", @"C\");
-
-
-
-            
-
-
-
-
-
-
-            Assembly localisationAssembly = Assembly.Load("ConsoleFileManager");
-            ResourceManager resourceManager = new ResourceManager("ConsoleFileManager.Properties.Resources", localisationAssembly);
-
-            //ResourceManager resourceManager = new ResourceManager("Resources.resx", typeof(UIClass).Assembly);
-
-            _firstPath = resourceManager.GetString("FristPath");
-            Console.WriteLine(_firstPath);
-            _userLastPath = resourceManager.GetString("UserLastPath");
-            Console.WriteLine(_userLastPath);
-
-            Console.WriteLine();
-
-            DirectoryInfo directoryInfo = new DirectoryInfo(@"E:\GeekBrains\Base C#\Algorimts\AlgoritmsLesson6");
-
-            foreach (var dirInf in directoryInfo.GetDirectories())
-            {
-                Console.WriteLine(dirInf.Name);
-            }
-
-            foreach(var dirInf in directoryInfo.GetFiles())
-            {
-                Console.WriteLine(dirInf.Name);
-            }
-            Console.ReadLine();
         }
 
-        private void UpdateAppConfigFile(string key,string value)
+        /// <summary>
+        /// Обновление либо добавление параметра в файл конфигурации
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        private void UpdateAppConfigFile(string key, string value)
         {
             try
             {
+                //Получение файла конфигурации исполняемого файла
                 var configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
                 var settings = configFile.AppSettings.Settings;
+
                 if (settings[key] == null)
                 {
                     settings.Add(key, value);
@@ -95,14 +96,62 @@ namespace ConsoleFileManager
                 {
                     settings[key].Value = value;
                 }
+
                 ConfigurationManager.RefreshSection(configFile.ConnectionStrings.SectionInformation.SectionName);
                 configFile.Save(ConfigurationSaveMode.Modified);
-                //ConfigurationManager.RefreshSection(configFile.AppSettings.SectionInformation.Name);
             }
             catch (ConfigurationErrorsException)
             {
-                Console.WriteLine("Error writing app settings");
+                Console.WriteLine("Обновить или добавить параметр в файл конфигурации не удалось!");
             }
+        }
+
+        /// <summary>
+        /// Печать содержимого папки
+        /// </summary>
+        /// <param name="folderPath">Адрес папки, содержимое которой необходимо отобразить</param>
+        private void PrintFolderContent(string folderPath)
+        { 
+            Console.Clear();
+
+            DirectoryInfo directoryInfo = new DirectoryInfo(@"" + folderPath);
+
+            buffer = new List<string>();
+            
+            foreach (var dirInf in directoryInfo.GetDirectories())
+            {
+                buffer.Add(dirInf.Name);
+            }
+            foreach (var dirInf in directoryInfo.GetFiles())
+            {
+                buffer.Add(dirInf.Name);
+            }
+
+            PrintSectionContent();
+
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Печать ограниченного кол-ва строк информации на странице
+        /// </summary>
+        private void PrintSectionContent()
+        {
+            if (buffer.Count == 0)
+                return;
+
+            int x = 0, y = 0;
+            int sNum = numPage * _countRowToView;
+
+            for (int i = sNum; i < (numPage + 1) * _countRowToView && i < buffer.Count; i++)
+            {
+                Console.SetCursorPosition(x + 3, y + 2);
+                Console.WriteLine(buffer[i]);
+                y++;
+            }
+
+            //Увеличение значения страницы с содержимым, которая отображена
+            numPage++;
         }
     }
 }
